8a003762a879ccc01ca7afc17b49411eb5cb511e
Chandan Sharma <chandan202.alive@gmail.com>
2022-10-04 13:59:43 +0530

    Update DBConnect.py

--- DBConnect.py
+++ DBConnect.py
@@ -1,22 +1,45 @@
 import csv 
 import pandas as pd
 import numpy as np
-
+import seaborn as sns
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+from matplotlib import pyplot as plt 
 # print("Hello World")
+from warnings import filterwarnings
 
-hackData = pd.read_csv("E:\PythonWorkbook\Data\Hack.csv")
+filterwarnings('ignore')
+
+hackData = pd.read_csv("E:\PythonWorkbook\PythonDataAnalysisWithPandasNumpySeabourn\Data\Hack.csv")
 
 
 
 pd.to_numeric(hackData["TotalScores"])
 # print("HALLOOOOO")
-print(hackData.info())
+# print(hackData.info())
 
 # print(hackData.info())
 
-hackGroupby = hackData.groupby("GroupID").agg({ "GroupID": np.size, "TotalScores": np.mean})
+hackGroupby = hackData.groupby("GroupID").agg({"TotalScores": np.mean, "GroupID": np.size})
 
-print(hackGroupby.describe())
+# print(hackGroupby)
+
+hackGroupby = hackGroupby.rename(columns={'GroupID': 'Number Group Members', 'TotalScores': 'Total (Out of 15)'})
+
+
+
+
+sns.set(style="darkgrid", color_codes = True)
+j = sns.jointplot(data = hackGroupby, x="Total (Out of 15)",y = "Number Group Members",kind='reg',color='g')
+r,p = stats.pearsonr(hackGroupby['Total (Out of 15)'], hackGroupby['Number Group Members'])
+
+print(r)
+# sns.histplot(hackGroupby.TotalScores,kde=True) 
+plt.show()
+
+hackGroupby = hackGroupby.rename(columns={'Number Group Members': 'GroupNum', 'Total (Out of 15)': 'Total'})
+ 
+print(hackGroupby.sort_values(by=['Total'], ascending=False))
 
 # gf = df.groupby('GroupID')TotalScores
 # gff = gf.first()
