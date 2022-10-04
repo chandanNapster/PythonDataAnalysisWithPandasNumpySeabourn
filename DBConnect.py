39849f149126dc56607653c839667ab7963b762d
Chandan Sharma <chandan202.alive@gmail.com>
2022-10-04 15:58:11 +0530

    Analysis

--- DBConnect.py
+++ DBConnect.py
@@ -24,23 +24,32 @@ hackGroupby = hackData.groupby("GroupID").agg({"TotalScores": np.mean, "GroupID"
 
 # print(hackGroupby)
 
-hackGroupby = hackGroupby.rename(columns={'GroupID': 'Number Group Members', 'TotalScores': 'Total (Out of 15)'})
+hackGroupby = hackGroupby.rename(columns={'GroupID': 'Number of Group Members', 'TotalScores': 'Total Marks (Out of 15)'})
 
 
 
 
 sns.set(style="darkgrid", color_codes = True)
-j = sns.jointplot(data = hackGroupby, x="Total (Out of 15)",y = "Number Group Members",kind='reg',color='g')
-r,p = stats.pearsonr(hackGroupby['Total (Out of 15)'], hackGroupby['Number Group Members'])
+j = sns.jointplot(data = hackGroupby, x="Total Marks (Out of 15)",y = "Number of Group Members",kind='reg', color='#4169e1')
+r,p = stats.pearsonr(hackGroupby['Total Marks (Out of 15)'], hackGroupby['Number of Group Members'])
 
 print(r)
 # sns.histplot(hackGroupby.TotalScores,kde=True) 
 plt.show()
 
-hackGroupby = hackGroupby.rename(columns={'Number Group Members': 'GroupNum', 'Total (Out of 15)': 'Total'})
+hackGroupby = hackGroupby.rename(columns={'Total Marks (Out of 15)': 'Total'})
  
-print(hackGroupby.sort_values(by=['Total'], ascending=False))
+final_result = hackGroupby.sort_values(by=['Total'], ascending=False).reset_index()
+
+print(final_result)
+
+final_result.to_csv('final_result.csv', index=False)
+
+
+
 
 # gf = df.groupby('GroupID')TotalScores
 # gff = gf.first()
 # print(gff.sort_values(by=['TotalScores']))
+
+
